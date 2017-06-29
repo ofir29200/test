0834c57a88ec3de1dab4dc3c03161e119fd6100a
ofir aviel <ofir29200@gmail.com>
2017-06-29 14:48:55 +0300

    fixed!

--- test.py
+++ test.py
@@ -1,10 +1,10 @@
 print ("ofir's testing project is loading")
 print ("hello world!")
 raw_input ("this program is going to ask you some questions, click enter to continue, otherwise close the window ")
-name = input ("what is your name? ")
-age = input ("what is your age? ")
-email = input ("what is your email? ")
-input("thank you for the info, click enter to continue to review your info")
-print ("your name is ", name)
-print ("\n your age is ", age)
-print ("\n your email is ", email)  
+name = raw_input ("what is your name? ")
+age = raw_input ("what is your age? ")
+email = raw_input ("what is your email? ")
+raw_input("thank you for the info, click enter to continue to review your info")
+print 'your name is ', name
+print 'your age is ', age
+print 'your email is ', email  
